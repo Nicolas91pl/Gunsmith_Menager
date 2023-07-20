ded55b7123dcb61b5f341dd4e9b4e07912649027
Patryk Miścior <nicolas91pl@o2.pl>
2023-07-20 20:05:26 +0200

    1st model

--- gun_work_menager/gunsmith/models.py
+++ gun_work_menager/gunsmith/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Licences(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    serial_number = models.CharField(max_length=60, unique=True)
+
+    conc = 'conc'
+    sport = 'sprt'
+    coll = 'coll'
+    self_def = 'self'
+    hunt = 'hunt'
+    Licences_Type_Choices = [
+        (conc, 'Koncesja'),
+        (sport, 'Sport'),
+        (coll, 'Kolekcjonerska'),
+        (self_def, 'Ochrona Osobista'),
+        (hunt, 'Myśliwska')
+    ]
+    type = models.CharField(max_lenght=4, choices=Licences_Type_Choices, blank=False)
+    realise_date = models.DateField()
+    authority = models.CharField(max_length=120)
+
